94532714c6b99ea23a49c3cce871f362fdc3608d
Emmy Kenduiwa <ckenduiwa9876@gmail.com>
2023-05-24 09:52:16 -0700

    String functions

---
+++ string_functions.c
@@ -0,0 +1,116 @@
+#include "shell.h"
+
+/**
+ * str_dup - Returns a pointer to a newly-allocated space in memory
+ * containing a copy of the string given as parameter.
+ * @string: The string to be copied.
+ *
+ * Return: If str == NULL or insufficient memory is available - NULL.
+ * Otherwise - a pointer to the duplicated string.
+ */
+char *str_dup(char *string)
+{
+	int i;
+	int length;
+	char *dup;
+
+	if (string == NULL)
+		return (NULL);
+
+	for (i = 0; *(string + i); i++)
+		length++;
+
+	dup = malloc(sizeof(char) * (length + 1));
+
+	if (dup == NULL)
+		return (NULL);
+
+	for (i = 0; *(string + i); i++)
+		*(dup + i) = *(string + i);
+
+	*(dup + length) = '\0';
+
+	return (dup);
+}
+
+/**
+ * str_len - checks the length of a string
+ * @string: pointer to string to check
+ *
+ * Return: int length of the string
+ */
+
+int str_len(char *string)
+{
+	int len = 0;
+
+	while (*(string + len))
+		len++;
+	return (len);
+}
+
+/**
+ * _strncpy - Copies at most an inputted number
+ * of bytes from string src into dest.
+ * @destination: The buffer storing the string copy.
+ * @source: The source string.
+ * @n: The maximum number of bytes to copied from src.
+ *
+ * Return: A pointer to the resulting string dest.
+ */
+char *_strncpy(char *destination, char *source, int n)
+{
+	int i = 0;
+	int length_of_source = 0;
+
+	while (source[i++])
+		length_of_source++;
+
+	for (i = 0; source[i] && i < n; i++)
+		destination[i] = source[i];
+
+	for (i = length_of_source; i < n; i++)
+		destination[i] = '\0';
+
+	return (destination);
+}
+
+/**
+ * *str_cpy -  copies the string pointed to by src
+ * @destination: char type string
+ * @source: char type string
+ * Description: Copy the string pointed to by pointer `src` to
+ * the buffer pointed to by `dest`
+ * Return: Pointer to `dest`
+ */
+
+char *str_cpy(char *destination, char *source)
+{
+	int index = -1;
+
+	do {
+		index++;
+		*(destination + index) = *(source + index);
+	} while (*(source + index) != '\0');
+
+	return (destination);
+}
+
+/**
+ * *str_cat - concatenates @src to @dest
+ * @source: the source string to append to @dest
+ * @destination: the destiation string to be concatenated upon
+ * Return: pointer to the resulting string
+ */
+
+char *str_cat(char *destination, char *source)
+{
+	int i = 0;
+	int destination_length = 0;
+
+	while (destination_length[i++])
+		destination_length++;
+	for (i = 0; source[i]; i++)
+		destination[destination_length++] = source[i];
+	return (destination);
+}
---
+++ string_functions_2.c
@@ -0,0 +1,130 @@
+#include "shell.h"
+
+/**
+ * str_cmp - Compares two strings.
+ * @string_1: A pointer to the first string to be compared.
+ * @string_2: A pointer to the second string to be compared.
+ *
+ * Return: If string_1 < string_2, the negative difference of
+ * the first unmatched characters.
+ * If string_1 == string_2, 0.
+ * If string_1 > string_2, the positive difference of
+ * the first unmatched characters.
+ */
+int str_cmp(char *string_1, char *string_2)
+{
+	while (*string_1 && *string_2 && *string_1 == *string_2)
+	{
+		string_1++;
+		string_2++;
+	}
+
+	return (*string_1 - *string_2);
+}
+
+/**
+ * _strncmp - Compares at most the first n bytes of two strings.
+ * @str1: A pointer to the first string to be compared.
+ * @str2: A pointer to the second string to be compared.
+ * @n: number of bytes to compare
+ *
+ * Return: If str1 < str2, the negative difference of
+ * the first unmatched characters.
+ *         If str1 == str2, 0.
+ *         If str1 > str2, the positive difference of
+ *         the first unmatched characters.
+ */
+int _strncmp(char *str1, char *str2, size_t n)
+{
+	while ((*str1 && *str2) && (*str1 == *str2))
+	{
+		str1++;
+		str2++;
+		n--;
+	}
+
+	return (*str1 - *str2);
+}
+
+/**
+ * _strpbrk - Searches a string for any of a set of bytes.
+ * @str: The string to be searched.
+ * @accept: The set of bytes to be searched for.
+ *
+ * Return: If a set is matched - a pointer to the matched byte.
+ * If no set is matched - NULL.
+ */
+char *_strpbrk(char *str, char *accept)
+{
+	int i;
+
+	while (*str)
+	{
+		for (i = 0; *(accept + i); i++)
+		{
+			if (*str == *(accept + i))
+				return (str);
+		}
+
+		str++;
+	}
+	return ('\0');
+}
+
+/**
+ * _strndup - Returns a pointer to a newly-allocated space in memory
+ * containing a copy of the string given as parameter.
+ * @s: The string to be copied.
+ * @n: The maximum number of bytes to be copied.
+ *
+ * Return: If str == NULL or insufficient memory is available - NULL.
+ * Otherwise - a pointer to the duplicated string.
+ */
+char *_strndup(char *s, size_t n)
+{
+	char *dup;
+	size_t i, length = 0;
+
+	if (s == NULL)
+		return (NULL);
+
+	while (*(s + length))
+		length++;
+
+	if (n < length)
+		length = n;
+
+	dup = malloc(sizeof(char) * (length + 1));
+
+	if (dup == NULL)
+		return (NULL);
+
+	for (i = 0; index < length; i++)
+		*(dup + i) = *(s + i);
+
+	*(dup + length) = '\0';
+
+	return (dup);
+}
+
+/**
+ * str_chr - Locates a character in a string.
+ * @str: The string to be searched.
+ * @c: The character to be located.
+ *
+ * Return: If c is found - a pointer to the first occurence.
+ * If c is not found - NULL.
+ */
+
+char *str_chr(char *str, char c)
+{
+	int i;
+
+	for (i = 0; *(str + i) >= '\0'; i++)
+	{
+		if (*(str + i) == c)
+			return (str + i);
+	}
+
+	return ('\0');
+}
